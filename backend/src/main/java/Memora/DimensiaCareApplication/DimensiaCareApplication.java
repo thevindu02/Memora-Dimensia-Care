c15b0d0cf3685ac68532acfe13a51a88343af2a9
thevindu02 <dil.thevi@gmail.com>
2025-06-24 17:55:15 +0530

    Initial Project Structure Added to dev branch

---
+++ backend/src/main/java/Memora/DimensiaCareApplication/DimensiaCareApplication.java
@@ -0,0 +1,13 @@
+package Memora.DimensiaCareApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimensiaCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimensiaCareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Memora/DimensiaCareApplication/DimensiaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package Memora.DimensiaCareApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimensiaCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
