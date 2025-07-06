c4a3eaa3283600776c9e3e32283dba0279581004
Minha02 <fathumaminha@gmail.com>
2025-07-06 16:33:54 +0530

    Updated notification screen navigations

--- mobile_app/lib/screens/patient/patient_notification_screen.dart
+++ mobile_app/lib/screens/patient/patient_notification_screen.dart
@@ -69,10 +69,6 @@ class _PatientNotificationScreenState extends State<PatientNotificationScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.pop(context),
-        ),
         title: const Text(
           'Notification',
           style: TextStyle(
