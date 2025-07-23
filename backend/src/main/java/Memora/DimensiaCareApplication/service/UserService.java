4455e24ce9adce99b60d68a4464093cbb2838e92
osahdha <oshadha.dw@gmail.com>
2025-07-23 12:52:07 +0530

    done

--- backend/src/main/java/Memora/DimensiaCareApplication/service/UserService.java
+++ backend/src/main/java/Memora/DimensiaCareApplication/service/UserService.java
@@ -1,13 +1,13 @@
 package Memora.DimensiaCareApplication.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import Memora.DimensiaCareApplication.model.User;
 import Memora.DimensiaCareApplication.repository.UserRepository;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
