2db7a25b69169cdb1d6a89800b88b000e3fc905c
thevindu02 <dil.thevi@gmail.com>
2025-10-19 00:17:51 +0530

    small changes

--- mobile_app/windows/flutter/generated_plugins.cmake
+++ mobile_app/windows/flutter/generated_plugins.cmake
@@ -3,11 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
   app_links
+  cloud_firestore
   connectivity_plus
   file_selector_windows
   firebase_core
+  flutter_secure_storage_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
